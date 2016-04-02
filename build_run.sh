bd4a6b25f668f3ca4314779f8d81548a43f80f34
Robin Hübner <profan@prfn.se>
2016-04-03 00:46:30 +0200

    Initial Commit.

---
+++ build_run.sh
@@ -0,0 +1,2 @@
+ocamlbuild -I data -package dyp -use-ocamlfind src/main.native
+./main.native
---
+++ src/main.ml
@@ -0,0 +1,23 @@
+(* entry point for ocaml compiler, will do argument parsing and generally make the parts collaborate. *)
+
+open Grammar_parser
+open Grammar_lexer
+
+let call_with_channel (ch_open, ch_close) fn file =
+    let channel = ch_open file in
+    let result = fn channel in
+        ch_close channel;
+        result
+
+let call_with_output_channel fn file = call_with_channel (open_out, close_out) fn file
+let call_with_input_channel fn file = call_with_channel (open_in, close_in) fn file
+
+let main ch =
+    try
+        let lexbuf = Lexing.from_channel ch in
+        let token_stream = Grammar_parser.main Grammar_lexer.token lexbuf in
+            List.iter (fun (t, str) -> Printf.printf "%d %s" t str) token_stream
+    with Grammar_lexer.Eof ->
+        print_string "EOF";;
+
+call_with_input_channel (fun ch -> main ch) "tests/first.calc"
